c8f087729a18071103f5f25154715dc8bc8867a7
Moh-Gtn <72886616+Moh-Gtn@users.noreply.github.com>
2022-11-18 15:53:06 +0100

    GAMEFIELD

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,10 +19,35 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
-        scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
+        Button button1 = new Button("X");
+        Button button2 = new Button("0");
+        Button button3 = new Button("X");
+        Button button4 = new Button("0");
+        Button button5 = new Button("X");
+        Button button6 = new Button("0");
+        Button button7 = new Button("X");
+        Button button8 = new Button("0");
+        Button button9 = new Button("X");
+
+        GridPane root = new GridPane();
+
+        root.add(button1, 0, 0, 1, 1);
+        root.add(button2, 1, 0, 1, 1);
+        root.add(button3, 2, 0, 1, 1);
+        root.add(button4, 0, 1, 1, 1);
+        root.add(button5, 1, 1, 1, 1);
+        root.add(button6, 2, 1, 1, 1);
+        root.add(button7, 0, 2, 1, 1);
+        root.add(button8, 1, 2, 1, 1);
+        root.add(button9, 2, 2, 1, 1);
+        Scene scene = new Scene(root, 80, 75);
         stage.setScene(scene);
         stage.show();
+
+        //scene = new Scene(loadFXML("primary"));
+        //scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
+        //stage.setScene(scene);
+        //stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
