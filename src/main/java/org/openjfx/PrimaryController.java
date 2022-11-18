91414ce395d2a4379caf3001adc8b53513fea6a4
nicole1795 <nicoledupyt95@gmail.com>
2022-11-18 15:58:52 +0100

    maeven

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -2,9 +2,23 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class PrimaryController {
+   @FXML public TextField ipAddress;
 
+    @FXML
+    private void sendIPAddress() throws IOException {
+        try {
+            System.out.println(ipAddress.getCharacters());
+        }
+        catch (Exception e) {
+            System.err.println(e);
+           // e.printStackTrace(); // System.err.println(e);
+        }
+    }
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
